d0b3e0d39976e189a5a9434b4b86cfb6f59a300c
shahdyousefak <67276189+shahdyousefak@users.noreply.github.com>
2020-09-25 11:16:13 -0400

    Add files via upload

--- ReverseArray/demo.java
+++ ReverseArray/demo.java
@@ -3,12 +3,12 @@ package ReverseArray;
 public class demo {
 	
 	public static void main(String[] args) {
-		int[] nums = {1,2,4};
-		reversearray reverseArray = new reversearray();
+		int[] nums = {1,6,4,10,-7,22};
+		reversearray reverseArray = new reversearray();//obj in reversearray class
 		int[] revArr = reverseArray.reverseArray(nums);
 		
 		for(int i=0; i<revArr.length; i++) {
-			System.out.println(revArr[i]);
+			System.out.print(revArr[i] + " ");
 		}
 	}
 }
