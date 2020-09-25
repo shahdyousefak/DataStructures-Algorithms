2f12ce93b5446c2e3037346b29bdab141a4b4b37
shahdyousefak <67276189+shahdyousefak@users.noreply.github.com>
2020-09-25 11:33:20 -0400

    Add files via upload

--- Anagram/Anagram.java
+++ Anagram/Anagram.java
@@ -5,8 +5,12 @@ public class Anagram {
 	public boolean solve(char[] w1, char[] w2) {
 	if(w1.length != w2.length)
 		return false;
+	
+	//O(NlogN)
 	Arrays.sort(w1);
 	Arrays.sort(w2);
+	
+	//O(N)
 	for(int i=0; i<w1.length; i++) {
 		if(w1[i] != w2[i])
 			return false;
@@ -14,5 +18,6 @@ public class Anagram {
 	return true;
 	
 	}
+	//O(NlogN) + O(N) = O(NlogN)
 
 }
